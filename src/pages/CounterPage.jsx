9d8ea8653b471561f57a7379736dc2e4d4d2d10e
Ludovic Esperce <ludovic.esperce@afpa.fr>
2025-12-21 13:03:01 +0100

    Premier commit

---
+++ src/pages/CounterPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const CounterPage = () => {
+    /**
+     * TODO Déclaration de l'état stockant la valeur du compteur
+     * 
+     * Plus d'information sur la déclaration des useState : https://www.carlrippon.com/typed-usestate-with-typescript/
+     */
+
+
+    /**
+     * TODO implémenter Fonction permettant d'incrémenter l'état du compteur.
+     * 
+     * Utilisée en tant que "handler" d'un évènement "click".
+     * 
+     * Pour un exemple d'implémentation d'un bouton vous pourrez vous inspirer de l'article suivant : https://www.kindacode.com/article/react-typescript-handling-onclick-event#example-1:-button-onclick
+     */
+
+
+    /**
+     * TODO implémenter un fonction permettant d'incrémenter l'état du compteur
+     * 
+     * Utilisée en tant que "handler" d'un évènement "click".
+     */
+
+
+    /**
+     * On renvoie le JSX correspond à ce qui est à afficher
+     */
+    return (
+        <>
+            <h2>Compteur :</h2>
+            {/* TODO compléter le code affichant le nombre de clics et des boutons */}
+        </>
+    )
+}
+
+export default CounterPage;
